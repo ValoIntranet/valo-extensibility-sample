03d2004f06cb2d53dc16502a28710e0bb7cab557
Elio Struyf <info@estruyf.be>
2019-06-07 11:02:14 +0200

    Adding delay sample to the dynamic data source

--- src/extensions/extendValoUi/datasource/DynamicPagingDataSource.ts
+++ src/extensions/extendValoUi/datasource/DynamicPagingDataSource.ts
@@ -1,26 +1,35 @@
-import { BaseDataSourceProvider, PagingType } from "@valo/extensibility";
+import { BaseDataSourceProvider, PagingType, DataSourceData } from "@valo/extensibility";
 
 
 export class DynamicPagingDataSource extends BaseDataSourceProvider {
   private defaultArray: number[] = [1, 2, 3, 4, 5];
 
-  public async getData(lastUpdated: string) {
-    const newArray = this.defaultArray.map(i => `Item ${i}`);
-    console.log(newArray);
-    return {
-      items: newArray
-    };
+  public async getData(lastUpdated: string): Promise<DataSourceData> {
+    return new Promise<DataSourceData>(resolve => {
+      setTimeout(() => {
+        const newArray = this.defaultArray.map(i => `Item ${i}`);
+        console.log(newArray);
+        resolve({
+          items: newArray
+        } as DataSourceData);
+      }, 2000);
+    });
   }
 
-  public async getPagedData(pageNr: number) {
+  public async getPagedData(pageNr: number): Promise<DataSourceData> {
     if (pageNr === 0) {
       return this.getData(null);
     }
-    const newArray = this.defaultArray.map(i => `Item ${i+(this.defaultArray.length * pageNr)}`);
-    console.log(newArray);
-    return {
-      items: newArray
-    };
+
+    return new Promise<DataSourceData>(resolve => {
+      setTimeout(() => {
+        const newArray = this.defaultArray.map(i => `Item ${i+(this.defaultArray.length * pageNr)}`);
+        console.log(newArray);
+        resolve({
+          items: newArray
+        } as DataSourceData);
+      }, 500 * pageNr);
+    });
   }
 
   public getPagingType() {
